f28022957e2be7234d426d7edb9b12b036768b5b
VERDUN Joris <joris42350@yahoo.fr>
2019-11-28 08:33:53 +0100

    arbre.h

--- util/arbre.h
+++ util/arbre.h
@@ -11,30 +11,34 @@
 #define A_CSTE_E 2
 #define A_CSTE_R 3
 #define A_CSTE_C 4
+#define A_CSTE_S 5
 
-#define A_PLUS 5
-#define A_MOINS 6
-#define A_MULT 7
-#define A_DIV 8
+#define A_PLUS 6
+#define A_MOINS 7
+#define A_MULT 8
+#define A_DIV 9
 
-#define A_SUP 9
-#define A_SUP_EG 10
-#define A_INF 11
-#define A_INF_EG 12
-#define A_EG 13
-#define A_DIFF 14
+#define A_SUP 10
+#define A_SUP_EG 11
+#define A_INF 12
+#define A_INF_EG 13
+#define A_EG 14
+#define A_DIFF 15
 
-#define A_LISTE 15
-#define A_AFFECT 16
+#define A_LISTE 16
+#define A_AFFECT 17
 
-#define A_APPEL_FCT 17
-#define A_APPEL_PROC 18
+#define A_APPEL_FCT 18
+#define A_APPEL_PROC 19
 
-#define A_WHILE 19
-#define A_FOR 20
-#define A_IF 21
-#define A_LISTE_IF 22 
-#define A_LISTE_ELSE 23
+#define A_WHILE 20
+#define A_FOR 21
+#define A_IF 22
+#define A_LISTE_IF 23 
+#define A_LISTE_ELSE 24
+
+#define A_TRUE 25
+#define A_FALSE 26
 
 
 typedef struct noeud{
